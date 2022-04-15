3a2cd3ace98158bcc54f4f3a0cb086662bde6902
Aditya Gupta <adityagupta765469@gmail.com>
2022-04-15 19:04:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
 import Widgets from './components/Widgets';
 
+
 function App() {
   const [currentAccount, setCurrentAccount] = useState('');
   const [correctNetwork, setCorrectNetwork] = useState(false);
